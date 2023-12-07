91d5dec5bcd7a524257dae7299db555cc2ee5aac
Nick Nowak <nick.nowak@htwg-konstanz.de>
2023-12-07 17:48:58 +0100

    Added swagger
    (removed microservice package)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { GatewayModule } from './gateway/gateway.module';
 
 const PORT = 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cloud project API')
+    .setDescription('The Cloud project API description.')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT);
 }
 
