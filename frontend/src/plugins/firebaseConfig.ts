ebd8f81356b01f3bcf39c0b172d6a6d5042b9ac9
Nick Nowak <nick.nowak@htwg-konstanz.de>
2023-12-10 17:02:08 +0100

    Added firebase config

---
+++ frontend/src/plugins/firebaseConfig.ts
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//initialize firebase auth
+const auth = getAuth();
+
+export { app, auth };
