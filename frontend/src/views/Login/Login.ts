b2deffdd18c408b6365d4f8d4065f30acad79014
Nick Nowak <nick.nowak@htwg-konstanz.de>
2023-12-12 14:58:28 +0100

    Added Register Button to login page

--- frontend/src/views/Login/Login.ts
+++ frontend/src/views/Login/Login.ts
@@ -1,11 +1,25 @@
+import CustomFooter from "@/components/CustomFooter/index.vue";
+import CustomAppBar from "@/components/CustomAppBar/index.vue";
+import { useDisplay } from "vuetify";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import router from "@/router";
+import { ref } from "vue";
+
 export default {
   name: "Login",
+  components: {
+    "custom-app-bar": CustomAppBar,
+    "custom-footer": CustomFooter,
+  },
   data() {
     return {
       // add variables
     };
   },
   setup() {
-    // do setup
+    const { xs } = useDisplay();
+    return {
+      xs,
+    };
   },
 };
