ba3cb640d00ce40d375f66db80738e6c0c985e17
TobiTgl <tobias_813@t-online.de>
2023-12-21 10:07:54 +0100

    test

--- backend/src/gateway/gateway.service.ts
+++ backend/src/gateway/gateway.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class GatewayService {
   public async hello(): Promise<String> {
-    return 'Hello! I am the API-Gateway.';
+    return 'Hello! I am the API-.';
   }
 }
