db1d96acb936d7c818699e5ce732be344d9a8476
Nick Nowak <nick.nowak@htwg-konstanz.de>
2024-01-10 10:04:08 +0100

    Created seperate Report and ListReport component

---
+++ frontend/src/components/ListReport.vue
@@ -0,0 +1,34 @@
+<template>
+  <vue-flex-waterfall
+    align-content="center"
+    col="5"
+    :break-at="breakAt"
+    col-spacing="15"
+    :break-by-container="true"
+  >
+    <report-vue
+      v-for="item in data"
+      :id="item.title"
+      :width="width"
+      :data="item"
+    />
+  </vue-flex-waterfall>
+</template>
+
+<script lang="ts" setup>
+import { VueFlexWaterfall } from "vue-flex-waterfall";
+import ReportVue from "@/components/Report.vue";
+
+defineProps({
+  data: { type: Array<any>, required: true },
+});
+
+const width = 200;
+const breakAt = (() => {
+  const obj: Record<string, number> = {};
+  for (let i = 1; i <= 5; i++) {
+    obj[width * (i + 1) + 15 * i] = i;
+  }
+  return obj;
+})();
+</script>
--- frontend/src/views/Dashboard/Dashboard.ts
+++ frontend/src/views/Dashboard/Dashboard.ts
@@ -1,11 +1,60 @@
+import { ref } from "vue";
+import ListReport from "@/components/ListReport.vue";
+
 export default {
   name: "Dashboard",
-  data() {
-    return {
-      // add variables
-    };
+  components: {
+    "list-report": ListReport,
   },
   setup() {
-    // do setup
+    const reports = ref();
+
+    // TODO: Replace with API call
+    reports.value = [
+      {
+        title: "Report 1",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies aliquam, nunc nisl ultricies nunc, vitae aliquet nisl nunc eget nisl. Donec auctor, nisl eget ultricies aliquam, nunc nisl ultricies nunc, vitae aliquet nisl nunc eget nisl.",
+      },
+      {
+        title: "Report 2",
+        text: "This is a report about something",
+      },
+      {
+        title: "Report 3",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies aliquam, nunc nisl ultricies nunc, vitae aliquet nisl nunc eget nisl. Donec auctor, nisl eget ultricies aliquam, nunc nisl ultricies nunc, vitae aliquet nisl nunc eget nisl.",
+      },
+      {
+        title: "Report 4",
+        text: "This is a report about something",
+      },
+      {
+        title: "Report 5",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies aliquam, nunc nisl ultricies nunc, vitae aliquet nisl nunc eget nisl. Donec auctor, nisl eget ultricies aliquam, nunc nisl ultricies nunc, vitae aliquet nisl nunc eget nisl.",
+      },
+      {
+        title: "Report 6",
+        text: "This is a report about something",
+      },
+      {
+        title: "Report 7",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies aliquam, nunc nisl ultricies nunc, vitae aliquet nisl nunc eget nisl. Donec auctor, nisl eget ultricies aliquam, nunc nisl ultricies nunc, vitae aliquet nisl nunc eget nisl.",
+      },
+      {
+        title: "Report 8",
+        text: "This is a report about something",
+      },
+      {
+        title: "Report 9",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies aliquam, nunc nisl ultricies nunc, vitae aliquet nisl nunc eget nisl. Donec auctor, nisl eget ultricies aliquam, nunc nisl ultricies nunc, vitae aliquet nisl nunc eget nisl.",
+      },
+      {
+        title: "Report 10",
+        text: "This is a report about something",
+      },
+    ];
+
+    return {
+      reports,
+    };
   },
 };
