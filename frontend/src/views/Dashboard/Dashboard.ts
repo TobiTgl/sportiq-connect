fd6ad86794fe3f7b9db97f2eebb246c3dbddecdf
Nick Nowak <nick.nowak@htwg-konstanz.de>
2024-01-10 11:41:23 +0100

    Added loading and error handeling to dashboard

--- frontend/src/views/Dashboard/Dashboard.ts
+++ frontend/src/views/Dashboard/Dashboard.ts
@@ -9,11 +9,22 @@ export default {
   },
   setup() {
     const reports = ref();
+    const loading = ref(true);
+    const showAlert = ref(true);
+    const alertType = ref<"error" | "success" | undefined>(undefined);
+    const alertMessage = ref("Getting reports");
 
     onMounted(() => {
       getAllReports()
         .then((data) => {
           reports.value = data;
+          loading.value = false;
+          showAlert.value = false;
+        })
+        .catch((error) => {
+          alertType.value = "error";
+          alertMessage.value = error.message;
+          loading.value = false;
         });
     });
 
