a9a3cc9194a838bd3e96c5bedc137a2168f2c509
Nick Nowak <nick.nowak@htwg-konstanz.de>
2024-01-10 11:36:13 +0100

    Updated Dashboard to get reports from backend

--- frontend/src/views/Dashboard/Dashboard.ts
+++ frontend/src/views/Dashboard/Dashboard.ts
@@ -1,5 +1,6 @@
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import ListReport from "@/components/ListReport.vue";
+import { getAllReports } from "@/services/reportService";
 
 export default {
   name: "Dashboard",
@@ -9,52 +10,19 @@ export default {
   setup() {
     const reports = ref();
 
-    // TODO: Replace with API call
-    reports.value = [
-      {
-        title: "Report 1",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies aliquam, nunc nisl ultricies nunc, vitae aliquet nisl nunc eget nisl. Donec auctor, nisl eget ultricies aliquam, nunc nisl ultricies nunc, vitae aliquet nisl nunc eget nisl.",
-      },
-      {
-        title: "Report 2",
-        text: "This is a report about something",
-      },
-      {
-        title: "Report 3",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies aliquam, nunc nisl ultricies nunc, vitae aliquet nisl nunc eget nisl. Donec auctor, nisl eget ultricies aliquam, nunc nisl ultricies nunc, vitae aliquet nisl nunc eget nisl.",
-      },
-      {
-        title: "Report 4",
-        text: "This is a report about something",
-      },
-      {
-        title: "Report 5",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies aliquam, nunc nisl ultricies nunc, vitae aliquet nisl nunc eget nisl. Donec auctor, nisl eget ultricies aliquam, nunc nisl ultricies nunc, vitae aliquet nisl nunc eget nisl.",
-      },
-      {
-        title: "Report 6",
-        text: "This is a report about something",
-      },
-      {
-        title: "Report 7",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies aliquam, nunc nisl ultricies nunc, vitae aliquet nisl nunc eget nisl. Donec auctor, nisl eget ultricies aliquam, nunc nisl ultricies nunc, vitae aliquet nisl nunc eget nisl.",
-      },
-      {
-        title: "Report 8",
-        text: "This is a report about something",
-      },
-      {
-        title: "Report 9",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies aliquam, nunc nisl ultricies nunc, vitae aliquet nisl nunc eget nisl. Donec auctor, nisl eget ultricies aliquam, nunc nisl ultricies nunc, vitae aliquet nisl nunc eget nisl.",
-      },
-      {
-        title: "Report 10",
-        text: "This is a report about something",
-      },
-    ];
+    onMounted(() => {
+      getAllReports()
+        .then((data) => {
+          reports.value = data;
+        });
+    });
 
     return {
       reports,
+      loading,
+      showAlert,
+      alertType,
+      alertMessage,
     };
   },
 };
