49e76eebd2e9e9ff5e5c961f0ad5ad7b9e9e6678
Nick Nowak <nick.nowak@htwg-konstanz.de>
2024-01-17 10:52:14 +0100

    Added Admin Page

---
+++ frontend/src/views/Admin/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container fluid class="admin">
+    <h1>Admin Page</h1>
+
+    <!-- loading alert (waiting on confirmation if user is a admin) -->
+    <v-container fluid v-if="loading">
+      <v-row class="fill-heigh" align-content="center" justify="center">
+        <v-col class="text-center" cols="12">
+          <v-alert type="info">message</v-alert>
+        </v-col>
+        <v-col cols="12" v-if="loading">
+          <v-progress-linear
+            color="primary"
+            rounded
+            height="6"
+            indeterminate
+          ></v-progress-linear>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <template v-else>
+      <span>Welcome to admin page! </span>
+    </template>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import router from "@/router";
+import { getAuth } from "firebase/auth";
+import { ref } from "vue";
+
+const loading = ref(true);
+const auth = getAuth();
+
+// check if user is admin and if not redirect to home page
+if (
+  (auth.currentUser as any).isAdmin === false ||
+  (auth.currentUser as any).isAdmin === undefined
+) {
+  router.push({ name: "Home" });
+}
+</script>
+
+<style lang="scss"></style>
