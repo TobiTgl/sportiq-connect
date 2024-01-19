53aa9503ea362975dad365c3c67016fcfc816954
Tobi Tögel <71888952+TobiTgl@users.noreply.github.com>
2024-01-19 23:42:19 +0100

    number animation

--- frontend/src/components/DailyReport.vue
+++ frontend/src/components/DailyReport.vue
@@ -1,9 +1,8 @@
 <template>
-  <v-container fluid class="text-center">
+  <v-container fluid class="text-center report-container">
     <h2>Reports created in the last 24 hours:</h2>
-    <transition name="fade">
-      <h2 class="number" :key="dailyReport">{{ dailyReport }}</h2>
-    </transition>
+    <h2 class="number">{{ displayedReport }}</h2>
+    <h2 class="number">{{ tweened.number.toFixed(0) }}</h2>
   </v-container>
 </template>
 
@@ -12,13 +11,38 @@ import { useDisplay } from "vuetify";
 import { getAuth } from "firebase/auth";
 import axios from "axios";
 import { getReportServiceUrl } from "@/helpers/helpers";
-import { onBeforeMount, ref } from "vue";
+import { onBeforeMount, reactive, ref, watch } from "vue";
 
 const { xs, smAndDown } = useDisplay();
 const dailyReport = ref(0);
+const displayedReport = ref(0);
 
 const auth = getAuth();
 const user = auth.currentUser;
+const tweened = reactive({
+  number: 0,
+});
+
+watch(dailyReport, (newVal, oldVal) => {
+  const duration = 2000;
+  const difference = newVal - oldVal;
+  let start = Date.now();
+
+  const easeInOutQuad = (t: number) =>
+    t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+
+  const step = () => {
+    const elapsed = Math.min(Date.now() - start, duration);
+    const t = elapsed / duration; // Normalized time (0.0 to 1.0)
+    displayedReport.value = Math.floor(easeInOutQuad(t) * difference + oldVal);
+
+    if (elapsed < duration) {
+      window.requestAnimationFrame(step);
+    }
+  };
+
+  window.requestAnimationFrame(step);
+});
 
 onBeforeMount(async () => {
   user?.getIdToken().then((token) => {
@@ -28,7 +52,7 @@ onBeforeMount(async () => {
       })
       .then((res) => {
         console.log(res.data);
-        dailyReport.value = res.data;
+        dailyReport.value = 100;
       })
       .catch((error) => {
         // error handling when Strava auth data could not be saved
@@ -38,16 +62,21 @@ onBeforeMount(async () => {
 });
 </script>
 <style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 2s;
-}
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-}
 .number {
-  color: #42b983; /* Vue green */
+  color: #48a9a6; /* Vue green */
   font-size: 3em;
+}
+.report-container {
+  background-color: #f9f9f9; /* Light gray background */
+  border: 1px solid #ddd; /* Gray border */
+  border-radius: 10px; /* Rounded corners */
+  padding: 20px; /* Some padding */
+  max-width: 600px; /* Maximum width */
+  margin: 0 auto; /* Center the container */
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Slight shadow for depth */
+}
+.report-container:hover {
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+  transition: box-shadow 0.3s ease-in-out;
 }
 </style>
