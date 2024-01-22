cb0b31b111342e3760425a1d6245871fe8a9c1c2
Tobi Tögel <71888952+TobiTgl@users.noreply.github.com>
2024-01-22 17:50:27 +0100

    env im footer & workflow variable

--- frontend/src/components/CustomFooter/CustomFooter.ts
+++ frontend/src/components/CustomFooter/CustomFooter.ts
@@ -3,8 +3,10 @@ import pjson from "../../../package.json";
 export default {
   name: "customFooter",
   data() {
+
     return {
       appVersion: pjson.version,
+      envVariable: import.meta.env.VITE_ENV,
     };
   },
 };
